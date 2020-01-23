15c35880d071c4e3d93ceea5ad68b600c573e129
amwebexpert <amwebexpert@gmail.com>
2020-01-22 19:17:42 -0500

    Added onClick with hook example

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import Axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { UserActions } from './actions/user.actions';
 import './App.css';
@@ -22,10 +22,10 @@ const App: React.FunctionComponent<{}> = (props) => {
   }
   const [data, setData] = useState(initialState);
 
-  const fetchUsers = async () => {
+  const fetchUsers = async (id: string) => {
     try {
       setData({ userState: data.userState, isFetching: true });
-      const response = await Axios.get('https://jsonplaceholder.typicode.com/users/2');
+      const response = await Axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
       setData({ userState: response.data, isFetching: false });
 
       dispatch({ type: UserActions.USER_ACTION_DATA_RETRIEVED, payload: response.data });
@@ -36,12 +36,33 @@ const App: React.FunctionComponent<{}> = (props) => {
   };
 
   useEffect(() => {
-    fetchUsers();
+    fetchUsers('4');
   }, [/** list of dependencies that will be causing the re-render */]);
+
+  // ------------------------------------------------------------------------------------------------------------------------------------
+  // https://stackoverflow.com/questions/55647287/how-to-send-request-on-click-react-hooks-way/55647571#55647571
+  const [isSending, setIsSending] = useState(false)
+  const sendRequest = useCallback(async () => {
+    // don't send again while we are sending
+    if (isSending) {
+      return;
+    }
+
+    // update state
+    setIsSending(true);
+
+    // send the actual request
+    await fetchUsers('8');
+
+    // once the request is sent, update state again
+    setIsSending(false);
+  }, [isSending]) // update the callback if the state changes
+  // ------------------------------------------------------------------------------------------------------------------------------------
 
   return (
     <div className="body-content">
       <p>Welcome {JSON.stringify(data)}</p>
+      <button disabled={isSending} onClick={sendRequest}>Another user</button>
       <StoreEcho />
     </div>
   );
